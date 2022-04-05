584aafdaea47a8316eb3d994ad4fd11872ee4672
unknown <sdlbsso@gmail.com>
2022-04-05 12:18:42 -0300

    comunicacion componente padre a hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'portfolio';
-  public cards: Array<any> = []
+  public listaVideos: Array<any> = []
 
   ngOnInit(): void {
-    this.cards = [
+    this.listaVideos = [
       {
         tittle: 'Video 1',
-        subtittle: 'Subtitulo del video 1'
+        subtittle: 'Subtitulo del video 1',
+        img: "https://i.ytimg.com/vi/3USuGvpZYsc/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC19ilijlch80j9aKrue-lhPqczjw"
       },
       {
         tittle: 'Video 2',
-        subtittle: 'Subtitulo del video 2'
+        subtittle: 'Subtitulo del video 2',
+        img:"https://i.ytimg.com/vi/ozv8ugNm0P0/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDc0jifW3dRngZuFFKSFg2-Sl6L7Q"
       },
       {
         tittle: 'Video 3',
-        subtittle: 'Subtitulo del video 3'
+        subtittle: 'Subtitulo del video 3',
+        img:"https://i.ytimg.com/vi/MTmZJ4euPes/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAypgKtRcwIzupvijfL8fyPu1ujMQ"
       }
     ]
   }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input() dataEntrante:any;
+
   public image:string;
   constructor() { }
 
   ngOnInit(): void {
-    this.image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRKgGYbcXiVqr_p0XoI5Coee_ld2j3fjEGVA&usqp=CAU"
+    this.image="https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Pink_Floyd_-_The_Endless_River_%28Artwork%29.jpg/220px-Pink_Floyd_-_The_Endless_River_%28Artwork%29.jpg"
   }
 
 }
